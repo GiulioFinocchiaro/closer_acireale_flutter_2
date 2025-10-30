c3fe6f68c82da9ff1e246789d245ef92969e540c
Giulio FInocchiaro <giuliofinocchiaro@MacBook-Pro-di-Giulio.local>
2025-10-30 17:47:59 +0100

    Update

--- closer_acireale_flutter/lib/main.dart
+++ closer_acireale_flutter/lib/main.dart
@@ -2,12 +2,19 @@ import 'package:closer_acireale_flutter/app_wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Inizializza i dati locali per la lingua desiderata
   await initializeDateFormatting('it_IT', null);
+  doWhenWindowReady(() {
+    final initialSize = Size(800, 600); // puoi ignorarlo
+    appWindow.size = initialSize;
+    appWindow.maximize(); // <-- Questo manda la finestra a fullscreen
+    appWindow.show();
+  });
   runApp(const CloserAcirealeApp());
 }
 
